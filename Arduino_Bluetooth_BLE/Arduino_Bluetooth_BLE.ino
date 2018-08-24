c64b2397bd7370e867044712ee0601039054b246
Spuds044 <38471895+Spuds044@users.noreply.github.com>
2018-08-24 11:39:17 +0100

    Update Arduino_Bluetooth_BLE.ino

--- Arduino_Bluetooth_BLE/Arduino_Bluetooth_BLE.ino
+++ Arduino_Bluetooth_BLE/Arduino_Bluetooth_BLE.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 //This is a very simple sketch for chatting with the iPhone, iPad, or Desktop
-//It will get a HELLO command to turn the light on or GOODBYE to turn it off.
+//It will get a command to turn the light on or a seperate command to turn it off.
 //AT commands can be sent through the IDE Monitor command. Or just data can be sent back to the phone.
 SoftwareSerial BLE_Serial(3, 4); // RX, TX
 char inData[64]; // Much larger than any expected command
@@ -38,13 +38,13 @@ void loop() // run until the end of time
         index++;
       }
     }
-    if(command==true && String(inData)=="HELLO")//Let's turn on the light
+    if(command==true && String(inData)=="INCOMING SIGNAL")//Let's turn on the light
     {
       command=false;//Reset for the next command
       digitalWrite(9, HIGH);//Turn on the really bright light
       Serial.println(String(inData));//Write out the assembled command
     }
-    else if(command==true && String(inData)=="GOODBYE")//Let's turn it off.
+    else if(command==true && String(inData)=="GOODBYE")//Let's start flashing.
     {
       command=false;//Reset for the next comment
       digitalWrite(9, LOW);//Blessed darkness
